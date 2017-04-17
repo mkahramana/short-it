b97e06b565bb13efabe80df08265e424d7edcb09
Mehmet Akif Kahraman <mehmetakifkahramana@gmail.com>
2017-04-17 21:25:29 +0300

    encode done

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from string import ascii_letters, digits
+
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
@@ -10,6 +12,8 @@ app.config.update({
 })
 db = SQLAlchemy(app)
 
+base = ascii_letters + digits
+
 
 class Link(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -19,25 +23,44 @@ class Link(db.Model):
 
 @app.route('/')
 def index():
-    a=Link.query.all()
-    return render_template("index.html",a=a)
+    a = Link.query.all()
+    return render_template("index.html", a=a)
 
 
 @app.route('/', methods=['POST'])
 def form_post():
     if request.method == 'POST':
         text = request.form['link']
-        save_to_db(text, '122456')
+        base_encode=encode()
+        save_to_db(text, base_encode)
     return render_template("index.html")
 
 
 def save_to_db(url, sorted_link):
-    domain=Link.query.filter_by(link=url).first()
+    domain = Link.query.filter_by(link=url).first()
     if not domain:
-        domain=Link(link=url,shortedLink=sorted_link)
+        domain = Link(link=url, shortedLink=sorted_link)
         db.session.add(domain)
         db.session.commit()
         return redirect('/')
+
+
+def to_base_62():
+    number=len(Link.query.all())
+    digits = []
+    while number > 0:
+        remainder = number % 62
+        digits.append(remainder)
+        number = int(number / 62)
+    return digits[::-1]
+
+
+def encode():
+    digits=to_base_62()
+    encode_letters = ''
+    for i in digits:
+        encode_letters += base[i]
+    return encode_letters
 
 
 if __name__ == '__main__':
