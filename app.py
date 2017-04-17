a6c9ebbc0e018c7392f5b679b52496f197a48c9e
Mehmet Akif Kahraman <mehmetakifkahramana@gmail.com>
2017-04-17 23:50:04 +0300

    some variable name renamed

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ base = ascii_letters + digits
 
 class Link(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    link = db.Column(db.String(255), unique=True)
-    shortedLink = db.Column(db.String(6), unique=True)
+    url = db.Column(db.String(255), unique=True)
+    shortenedURL = db.Column(db.String(6), unique=True)
 
 
 @app.route('/')
@@ -30,35 +30,36 @@ def index():
 def form_post():
     if request.method == 'POST':
         text = request.form['link']
+        text = text.replace('http://', '')
+        text = text.replace('https://', '')
         if not text[-1] is '+':
-            text = text.replace('http://', '')
-            text = text.replace('https://', '')
-            is_db = Link.query.filter_by(link=text).first()
+            is_db = Link.query.filter_by(url=text).first()
             if not is_db:
                 base_encode = encode()
                 save_to_db(text, base_encode)
                 shortened_url = 'http://shorturldemo.com/' + base_encode
-                return render_template("index.html",shortened_url=shortened_url)
+                return render_template("index.html", shortened_url=shortened_url)
             else:
-                url = Link.query.filter_by(link=text).first().shortedLink
+                url = Link.query.filter_by(url=text).first().shortenedURL
                 shortened_url = 'http://shorturldemo.com/' + url
-                return render_template("index.html",shortened_url=shortened_url)
+                return render_template("index.html", shortened_url=shortened_url)
         else:
             try:
                 start = text.index('/')
                 url = text[start + 1:-1]
+                print(url)
                 base_decode = decode(url)
-                url = Link.query.filter_by(id=base_decode).first()
-                return redirect("http://" + url.link)
+                shortened_url = Link.query.filter_by(id=base_decode).first()
+                return redirect("http://" + shortened_url.url)
             except ValueError:
                 return 0
 
 
-def save_to_db(url, sorted_link):
-    domain = Link.query.filter_by(link=url).first()
-    if not domain:
-        domain = Link(link=url, shortedLink=sorted_link)
-        db.session.add(domain)
+def save_to_db(url, shortenedURL):
+    is_db = Link.query.filter_by(url=url).first()
+    if not is_db:
+        is_db = Link(url=url, shortenedURL=shortenedURL)
+        db.session.add(is_db)
         db.session.commit()
         return redirect('/')
 
@@ -66,11 +67,15 @@ def save_to_db(url, sorted_link):
 def to_base_62():
     number = len(Link.query.all())
     digits = []
-    while number > 0:
-        remainder = number % 62
-        digits.append(remainder)
-        number = int(number / 62)
-    return digits[::-1]
+    if number == 0:
+        digits.append(0)
+        return digits
+    else:
+        while number > 0:
+            remainder = number % 62
+            digits.append(remainder)
+            number = int(number / 62)
+        return digits[::-1]
 
 
 def encode():
